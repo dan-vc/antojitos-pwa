5d9f4ebc1cf09f8ff3f9b7bd47d68b79a8dbcac3
Daniel Villafranqui <132804140+dan-vc@users.noreply.github.com>
2024-09-03 01:38:53 -0500

    Delete Antojitos_del_Cielo_Web directory

--- Antojitos_del_Cielo_Web/main.js
+++
@@ -1,7 +0,0 @@
-let swLocation = 'sw.js';
-
-if (navigator.serviceWorker) {
-  if (window.location.href.includes('localhost')) swLocation = '/sw.js'
-
-  navigator.serviceWorker.register(swLocation)
-}
